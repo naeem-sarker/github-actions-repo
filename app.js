9edf20dc8294a9e5dd5bfc2e3b127ad5125bca8a
Naeem Sarker <naeemsarker85@gmail.com>
2025-09-19 00:40:48 +0600

    feat: Add Actions for GitHub

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.status(210).send("Hello World");
+    res.send("Hello World");
 })
 
 module.exports = app;
